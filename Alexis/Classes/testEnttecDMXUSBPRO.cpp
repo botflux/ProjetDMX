8f9af88adfe6146d3a18c15cd1b04868755d0bd2
Francisdu68 <35294071+Francisdu68@users.noreply.github.com>
2018-02-21 10:36:24 +0100

    Update testEnttecDMXUSBPRO.cpp
    Ajout fonction dans la classe

--- Alexis/Classes/testEnttecDMXUSBPRO.cpp
+++ Alexis/Classes/testEnttecDMXUSBPRO.cpp
@@ -56,11 +56,11 @@ int main(int argc, char *argv[])
 		string configurationDMX = interfaceDMX->GetConfiguration();
 		cout << "Interface DMX USB PRO detectee " << std::endl << configurationDMX << std::endl;
 
-		//On initialise le tableau des valeur a envoyer par voie DMX Ã  0
+		//On initialise le tableau des valeur a envoyer par voie DMX à 0
 		int valeurDMX[TAILLEBUSDMX];//tableau contenant les Valeur des 512 canaux du bus DMX
 		memset(valeurDMX,0x00, TAILLEBUSDMX);
 
-		//On crÃ©e et configure le socket en UDP avec le bon port et n'importe qu'elle interface reseau du raspberry(ici on a la clÃ© wi fi sur un rÃ©seau et la carte reseau sur un autre)
+		//On crée et configure le socket en UDP avec le bon port et n'importe qu'elle interface reseau du raspberry(ici on a la clé wi fi sur un réseau et la carte reseau sur un autre)
 		Socket *mySocket = new Socket();
 		mySocket->setDescripteur(PF_INET,SOCK_DGRAM,0);
 		mySocket->setPdrLocal(PF_INET, htons(PORT), htonl(INADDR_ANY));
@@ -69,14 +69,18 @@ int main(int argc, char *argv[])
 		vector<scene> storyboard;//On prepare le tableau dynamique qui contiendra les storyboard
 
 		char message[LG_MESSAGE];//On initialise le message que le serveur va recevoir
-
+		
 		while(1)
 		{
-			memset(message, 0x00, LG_MESSAGE*sizeof(char));//On le remet Ã  0 Ã  chaque fois
+			memset(message, 0x00, LG_MESSAGE*sizeof(char));//On le remet à 0 à chaque fois
+			//memset(reponse, 0x00, LG_MESSAGE*sizeof(char));
 			//On attend un ordre d'un client
 			mySocket->setLus(recvfrom(mySocket->getDescripteur(), message, sizeof(message),0,(struct sockaddr*)mySocket->getPdrDistant(), mySocket->getLgAddr2()));
 			//On affiche le message recu
 			cout << "Message recu : " << message << endl;
+			mySocket->setPdrDistant(0, htons(15000), 0);
+			mySocket->affichePdrDistant();
+			
 			string m = message;
 
 			//On creer un parser et on l'utilise pour decoder le message recu
@@ -91,12 +95,7 @@ int main(int argc, char *argv[])
 			}
 			string cible = "";
 
-			int valueRed=0;
-			int valueGreen=0;
-			int valueBlue=0;
-			int valueIntensity=0;
-
-			//On rempli un tableau en 2 dimension avec les information decodÃ©es du message qu'on trie
+			//On rempli un tableau en 2 dimension avec les information decodées du message qu'on trie
 			for(int i=0; i< fullDecoded.size(); i=i+1)
 			{
 				cout <<"Nom : " << fullDecoded[i][0]  <<"\tValeur : " << fullDecoded[i][1] << endl; 
@@ -105,35 +104,16 @@ int main(int argc, char *argv[])
 					cible = fullDecoded[i][1];//On cherche avant tout la cible du message
 				}
 			}
-			//string testProjo = ADRESSEDEBUTPROJO;
-			//string testLyre = ADRESSEDEBUTLYRE;
-			//cout << "testProjo = " << testProjo << " ; testLyre = " << testLyre << "\nCible = " << cible << endl;
 			//Si la cible est le projo
 			if (cible == ADDRPROJO)
 			{
-			for(int i=0; i < fullDecoded.size();i=i+1)
+				mySocket->sendACK(true);
+				for(int i=0; i < fullDecoded.size();i=i+1)
 				{
-					if(fullDecoded[i][0]=="RED")
-					{
-						valueRed = atoi(fullDecoded[i][1].c_str());
-					}
-					if(fullDecoded[i][0]=="BLUE")
-					{
-						valueBlue = atoi(fullDecoded[i][1].c_str());
-					}
-					if(fullDecoded[i][0]=="GREEN")
-					{
-						valueGreen = atoi(fullDecoded[i][1].c_str());
-					}
-				}
-				for(int i=0; i<=3;i=i+1)//La lyre est divisÃ©e en 4 quart, on veut allumer la barre entierrement donc on fait une boucle qui allume chaque quart
-				{
-					valeurDMX[ADRESSEDEBUTPROJO + (i*3)] = valueRed;
-					valeurDMX[ADRESSEDEBUTPROJO + 1 + (i*3)] = valueGreen;
-					valeurDMX[ADRESSEDEBUTPROJO + 2 + (i*3)] = valueBlue;
+					mySocket->remplirTab(valeurDMX, ADRESSEDEBUTPROJO, "PROJO", fullDecoded[i][0], fullDecoded[i][1]);
 				}
 
-				for(int i=ADRESSEDEBUTPROJO; i <=ADRESSEDEBUTPROJO+NBCANAUXPROJO; i=i+1)
+				for(int i=ADRESSEDEBUTPROJO; i <=ADRESSEDEBUTPROJO+NBCANAUXPROJO - 1; i=i+1)
 				{
 					interfaceDMX->SetCanalDMX(i, valeurDMX[i]);
 				}
@@ -143,34 +123,13 @@ int main(int argc, char *argv[])
 			//Si la cible est la lyre
 			else if(cible == ADDRLYRE)
 			{
-				for(int i=0; i < fullDecoded.size(); i=i+1)
+				mySocket->sendACK(true);
+				for(int i=0; i < fullDecoded.size();i=i+1)
 				{
-					if (fullDecoded[i][0]=="RED")
-					{
-						valueRed = atoi(fullDecoded[i][1].c_str());
-					}
-
-					if (fullDecoded[i][0]=="BLUE")
-					{
-						valueBlue = atoi(fullDecoded[i][1].c_str());
-					}
-
-					if (fullDecoded[i][0]=="GREEN")
-					{
-						valueGreen = atoi(fullDecoded[i][1].c_str());
-					}
-
-					if(fullDecoded[i][0]=="INTENSITY")
-					{
-						valueIntensity = atoi(fullDecoded[i][1].c_str());
-					}
+					mySocket->remplirTab(valeurDMX, ADRESSEDEBUTLYRE, "LYRE", fullDecoded[i][0], fullDecoded[i][1]);
 				}
-				valeurDMX[ADRESSEDEBUTLYRE]=valueRed;
-				valeurDMX[ADRESSEDEBUTLYRE+1]=valueGreen;
-				valeurDMX[ADRESSEDEBUTLYRE+2]=valueBlue;
-				valeurDMX[ADRESSEDEBUTLYRE+6]=valueIntensity;
-
-				for(int i=ADRESSEDEBUTLYRE; i<=ADRESSEDEBUTLYRE+6; i=i+1)
+				
+				for(int i=ADRESSEDEBUTLYRE; i<=ADRESSEDEBUTLYRE+NBCANAUXLYRE-1; i=i+1)
 				{
 					interfaceDMX->SetCanalDMX(i, valeurDMX[i]);
 				}
@@ -180,6 +139,7 @@ int main(int argc, char *argv[])
 			else//si il n'y a pas de cible ou qu'elle est incorrecte
 			{
 				cout<< "Cible incorrecte"<<endl;
+				mySocket->sendACK(false);
 			}
 		}
 		mySocket->closeDescripteur();
