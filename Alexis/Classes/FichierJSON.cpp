d140fa16ced8a7f0b4c6a8daf08f28912861a599
Francisdu68 <35294071+Francisdu68@users.noreply.github.com>
2018-03-23 08:23:59 +0100

    Create FichierJSON.cpp

---
+++ Alexis/Classes/FichierJSON.cpp
@@ -0,0 +1,118 @@
+#include "fichierJSON.h"
+#include <string>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+string fichierJSON::getName(json_value* value, string name)
+{
+	int length, x;
+	
+	if (value == NULL) 
+	{
+		cout << "value = NULL";
+		return "";
+	}
+	length = value->u.object.length;
+
+	for (x = 0; x < length; x++) 
+	{
+		string n = value->u.object.values[x].name;
+		string v = value->u.object.values[x].value->u.string.ptr;
+		
+		if(n == name)
+		{
+			return v;
+		}
+	}
+	return "test vzrgzio";
+}
+
+void fichierJSON::setConfig(json_value* value, int &port, int &portDistant, int &nbCanauxProjo, int &nbCanauxLyre, int &tailleBusDMX)
+{
+	if(getName(value, "PORT")!="")
+	{
+		port = atoi(getName(value, "PORT").c_str());
+	}
+	
+	if(getName(value, "PORTDISTANT")!="")
+	{
+		portDistant = atoi(getName(value, "PORTDISTANT").c_str());
+	}
+	
+	if(getName(value, "NBCANAUXPROJO")!="")
+	{
+		nbCanauxProjo = atoi(getName(value, "NBCANAUXPROJO").c_str());
+	}
+	
+	if(getName(value, "NBCANAUXLYRE")!="")
+	{
+		nbCanauxLyre = atoi(getName(value, "NBCANAUXLYRE").c_str());
+	}
+	
+	if(getName(value, "TAILLEBUSDMX")!="")
+	{
+		tailleBusDMX = atoi(getName(value, "TAILLEBUSDMX").c_str());
+	}
+}
+
+json_value* fichierJSON::ouvrirJSON(string fileName)
+{
+        char* filename = new char[fileName .length() + 1];
+		strcpy(filename , fileName .c_str());
+	
+        FILE* fp;
+        struct stat filestatus;
+        int file_size;
+        char* file_contents;
+        json_char* json;
+        json_value* value;
+
+        if ( stat(filename, &filestatus) != 0) {
+			cout << "File "<< filename <<" not found\n";
+			exit(0);
+        }
+        file_size = filestatus.st_size;
+        file_contents = (char*)malloc(filestatus.st_size);
+        if ( file_contents == NULL) {
+			cout << "Memory error: unable to allocate "<<file_size<<" bytes\n";
+			exit(0);
+        }
+
+        fp = fopen (filename, "r");
+        if (fp == NULL) {
+			cout << "Unable to open "<<filename<<endl;
+			fclose (fp);
+			free(file_contents);
+			exit(0);
+        }
+        if ( fread(file_contents, file_size, 1, fp) != 1 ) {
+			cout << "Unable t read content of "<<filename<<endl;
+			fclose (fp);
+			free(file_contents);
+			exit(0);
+        }
+        fclose (fp);
+
+        cout << file_contents << endl;
+
+        cout << "--------------------------------\n\n";
+
+        json = (json_char*)file_contents;
+
+        value = json_parse(json,file_size);
+
+        if (value == NULL) {
+                cout << "Unable to parse data\n";
+                free(file_contents);
+                exit(1);
+        }
+		free(file_contents);
+	return value;
+}
+
+void fichierJSON::fermerJSON(json_value* value)
+{
+	json_value_free(value);
+}
